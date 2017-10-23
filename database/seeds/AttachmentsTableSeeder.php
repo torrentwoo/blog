c59e6e084693f2d875c6fc5fe6b2f783c2678d8c
torrentwoo <790896@qq.com>
2017-10-23 10:45:18 +0800

    Update the seeder of model Attachment

--- database/seeds/AttachmentsTableSeeder.php
+++ database/seeds/AttachmentsTableSeeder.php
@@ -14,11 +14,11 @@ class AttachmentsTableSeeder extends Seeder
         // 以容器的方式调用测试数据生成器
         $faker = app(Faker\Generator::class);
         // 获取已发布文章的 id
-        $articlesIdArr = App\Models\Article::lists('id')->toArray();
-        // 为每篇文章随机分配 0 到 3 个测试的附件内容
-        foreach ($articlesIdArr as $id) {
+        $arrArticlesId = App\Models\Article::lists('id')->toArray();
+        // 为随机的文章分配随机个数的测试附件内容
+        foreach ($arrArticlesId as $id) {
             $amount = array_rand(range(0, 3));
-            if ($amount > 0) {
+            if ($amount > 1) {
                 $attachments = factory(App\Models\Attachment::class)->times($amount)->make()->each(function($ele) use ($faker, $id) {
                     $ele->article_id = $id;
                 })->toArray();
