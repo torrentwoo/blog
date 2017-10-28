94d3b282c903f9ca1bac88b9d0a1dd11e7c69062
torrentwoo <790896@qq.com>
2017-10-28 22:17:42 +0800

    Update shared footer template, added navigation at footer section

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,3 +1,8 @@
-        <footer>
-            <p>&copy; {{ date('Y') }} {{ $CopyrightOwner or 'Demo' }}</p>
+        <footer class="row">
+            <p class="col-xs-12 col-sm-6 col-md-8">&copy; {{ date('Y') }} {{ $CopyrightOwner or 'Demo' }}</p>
+            <ul id="footer-nav" class="col-xs-6 col-md-4 hidden-xs">
+                <li><a href="#">关于</a></li>
+                <li><a href="#">联系</a></li>
+                <li><a href="#">帮助</a></li>
+            </ul>
         </footer>
