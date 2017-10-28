1f8880d3ea933ba748e2c03fe8826439f4eb8633
torrentwoo <790896@qq.com>
2017-10-28 23:31:10 +0800

    Update shared nav template, changed nav-right section, added dropdown search

--- resources/views/shared/nav.blade.php
+++ resources/views/shared/nav.blade.php
@@ -15,6 +15,31 @@
                     <li><a href="#about">About</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#login">登陆</a></li>
+                    <li><a href="#logon">注册</a></li>
+                    <li><a href="#help">帮助</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                            <span class="sr-only">搜索</span>
+                        </a>
+                        <div id="nav-search" class="dropdown-menu">
+                            <form class="navbar-form" action="{{ route('search') }}" method="get" role="search">
+                                {{ csrf_field() }}
+                                <div class="input-group">
+                                    <input type="text" name="keyword" class="form-control" placeholder="Search for..." />
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-default" type="submit">
+                                            <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+                                        </button>
+                                    </span>
+                                </div>
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+{{--
                 <form class="navbar-form navbar-right" action="{{ route('search') }}" method="get">
                     {{ csrf_field() }}
                     <div class="input-group">
@@ -26,6 +51,7 @@
                         </span>
                     </div>
                 </form>
+--}}
             </div><!-- /.nav-collapse -->
         </div><!-- /.container -->
     </nav><!-- /.nav-element -->
