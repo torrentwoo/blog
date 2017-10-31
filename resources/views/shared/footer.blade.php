d6e2c3ee61db4bfc3dbad6e9731d87723b9762af
torrentwoo <790896@qq.com>
2017-10-31 18:02:37 +0800

    Update shared footer blade template, add named route for navigation

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,8 +1,8 @@
         <footer class="row">
             <p class="col-xs-12 col-sm-6 col-md-8">&copy; {{ date('Y') }} {{ $CopyrightOwner or 'Demo' }}</p>
             <ul id="footer-nav" class="col-xs-6 col-md-4 hidden-xs">
-                <li><a href="#">关于</a></li>
-                <li><a href="#">联系</a></li>
-                <li><a href="#">帮助</a></li>
+                <li><a href="{{ route('about') }}">关于</a></li>
+                <li><a href="{{ route('contact') }}">联系</a></li>
+                <li><a href="{{ route('help') }}">帮助</a></li>
             </ul>
         </footer>
