a831532a612c2d29bca5c5635a29e83f193b5d61
torrentwoo <790896@qq.com>
2017-10-31 14:08:31 +0800

    Add about blade template

---
+++ resources/views/layouts/about.blade.php
@@ -0,0 +1,32 @@
+@extends('shared.origin')
+
+@section('content')
+                <ol class="breadcrumb">
+                    <li><a href="/">首页</a></li>
+                    <li class="active">联系</li>
+                </ol>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="page-content">
+                            <h3>关于</h3>
+                            <p>Content goes here...</p>
+                        </div>
+                    </div>
+                </div>
+@stop
+
+{{--
+    Static pages, shared sidebar
+--}}
+@section('sidebar')
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h5 class="panel-title">快捷导航</h5>
+                    </div>
+                    <div class="list-group">
+                        <a class="list-group-item active" href="{{ route('about') }}">关于</a>
+                        <a class="list-group-item" href="{{ route('contact') }}">联系</a>
+                        <a class="list-group-item" href="{{ route('help') }}">帮助</a>
+                    </div>
+                </div>
+@stop
