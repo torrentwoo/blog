e376197cd48ba4439ef25cd4acd0bfc200a53243
torrentwoo <790896@qq.com>
2017-11-03 14:12:31 +0800

    Update nav blade template, do first step for positioning of navigation

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -21,7 +21,7 @@ class SessionsController extends Controller
      */
     public function create()
     {
-        return view('layouts.login');
+        return view('layouts.login')->with('loginActive', 'active');
     }
 
     /**
