1d661c6d6aee870d3ac126ea2c8a9b6bf42815bd
torrentwoo <790896@qq.com>
2017-11-07 22:05:11 +0800

    Update shared message blade template, added the status field for handle password rescue status report

--- resources/views/shared/message.blade.php
+++ resources/views/shared/message.blade.php
@@ -1,4 +1,4 @@
-@foreach (['success', 'info', 'warning', 'danger'] as $msg)
+@foreach (['info', 'status', 'success', 'warning', 'danger'] as $msg)
 @if (session()->has($msg))
         <div id="global-flash-message" class="alert alert-{{ $msg }} alert-dismissible" role="alert">
             <button aria-label="Close" data-dismiss="alert" class="close" type="button"><span aria-hidden="true">&times;</span></button>
