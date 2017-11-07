3f297dd3ed9ed6232036bc2a53e2e6c8a96e8f76
torrentwoo <790896@qq.com>
2017-11-07 16:42:22 +0800

    Update sessions controller, updated method store, applied friendly redirect via intended function

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -58,8 +58,7 @@ class SessionsController extends Controller
                 $nickname = $user->nickname;
                 $nickname = $nickname ?: $user->name;
                 session()->flash('success', "登录成功，欢迎您回来：{$nickname}");
-                //return redirect()->route('home')->with('user', $user);
-                return redirect()->intended(route('user.show', [$user]));
+                return redirect()->intended(route('user.show', compact('user')));
             } else {
                 Auth::logout();
                 session()->flash('warning', '您的账户未激活，请登陆您的注册邮箱，检查注册验证邮件以便激活您的账户');
