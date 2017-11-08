7db93953718a30d09840a893d591d4d4ae81e0b2
torrentwoo <790896@qq.com>
2017-11-08 19:17:49 +0800

    Update model user fillable property, added last_login_ip field for user login event

--- app/Models/User.php
+++ app/Models/User.php
@@ -28,7 +28,7 @@ class User extends Model implements AuthenticatableContract,
      *
      * @var array
      */
-    protected $fillable = ['name', 'email', 'password', 'nickname'];
+    protected $fillable = ['name', 'email', 'password', 'nickname', 'last_login_ip'];
 
     /**
      * The attributes excluded from the model's JSON form.
