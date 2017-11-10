33e40d19fe68e6eef0d0040d1cd35da6e93b478c
torrentwoo <790896@qq.com>
2017-11-10 11:00:10 +0800

    Update modal-login blade template, renamed key field though prefix ajax

--- resources/views/features/modal-login.blade.php
+++ resources/views/features/modal-login.blade.php
@@ -7,16 +7,16 @@
                                 </div>
                                 <div class="modal-body">
                                     <div class="form-group">
-                                        <label for="modal-username" class="control-label">帐号</label>
-                                        <input type="text" name="username" id="modal-username" class="form-control" placeholder="您的帐号" />
+                                        <label for="ajax-username" class="control-label">帐号</label>
+                                        <input type="text" name="username" id="ajax-username" class="form-control" placeholder="您的帐号" />
                                     </div>
                                     <div class="form-group">
-                                        <label for="modal-password" class="control-label">密码</label>
-                                        <input type="password" name="password" id="modal-password" class="form-control" placeholder="登录密码" />
+                                        <label for="ajax-password" class="control-label">密码</label>
+                                        <input type="password" name="password" id="ajax-password" class="form-control" placeholder="登录密码" />
                                     </div>
                                     <div class="checkbox">
                                         <label>
-                                            <input type="checkbox" name="remember" />
+                                            <input type="checkbox" name="remember" id="ajax-remember" />
                                             <span>记住我（下次自动登录）</span>
                                         </label>
                                         <span class="pull-right">没有帐号？前往<a href="{{ route('register') }}">注册新用户</a></span>
@@ -24,7 +24,7 @@
                                 </div>
                                 <div class="modal-footer">
                                     <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
-                                    <button type="button" id="loginModalSubmit" class="btn btn-primary">登录</button>
+                                    <button type="button" id="ajaxLoginSubmit" class="btn btn-primary">登录</button>
                                 </div>
                             </div>
                         </div>
