4fba4a7188653ec76a8e67d138e1773d3c4e6c16
torrentwoo <790896@qq.com>
2017-11-11 00:29:18 +0800

    Updated basic content, format

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -8,6 +8,26 @@
                 <div class="row">
                     <div class="col-xs-12">
                         <div class="page-content">
+                            <h1>内容合作</h1>
+                            <dl class="dl-horizontal">
+                                <dt>邮箱</dt>
+                                <dd>where@domain.suffix</dd>
+                                <dt>投稿须知</dt>
+                                <dd>
+                                    <ol class="list-unstyled">
+                                        <li>1、稿件内容请务必<em>以附件形式</em>发送到指定邮箱</li>
+                                        <li>2、您的投稿若被采用，本站会支付稿费；所以请您投稿时，备注您的收款方式</li>
+                                        <li>3、稿件内容以原创为优；非原创内容投稿，暂时不会有稿费，而是以<abbr title="在本站可用的礼品卡券，虚拟货币">网站代币</abbr>的方式划拨到您的账户</li>
+                                    </ol>
+                                </dd>
+                            </dl>
+                            <h2>反馈建议</h2>
+                            <dl class="dl-horizontal">
+                                <dt>在线反馈</dt>
+                                <dd><a href="#feedback">在线反馈</a></dd>
+                                <dt>邮箱</dt>
+                                <dd>mail@address.suffix</dd>
+                            </dl>
                             <h3>商务合作</h3>
                             <dl class="dl-horizontal">
                                 <dt>电话</dt>
@@ -15,19 +35,6 @@
                                 <dd>{{ env('APP_ENV') }}</dd>
                                 <dt>邮箱</dt>
                                 <dd>fakemailbox@fakedomain.suffix</dd>
-                            </dl>
-                            <h3>内容合作</h3>
-                            <dl class="dl-horizontal">
-                                <dt>邮箱</dt>
-                                <dd>where@domain.suffix</dd>
-                                <dt>投稿须知</dt>
-                                <dd>
-                                    <ul class="list-unstyled">
-                                        <li>稿件内容请务必以附件形式发送到指定邮箱</li>
-                                        <li>稿件内容若不是您原创，请务必备注来源</li>
-                                        <li>您的投稿若被采用，本站会支付稿费，所以请您投稿时，备注您的收款方式</li>
-                                    </ul>
-                                </dd>
                             </dl>
                             <h3>广告合作</h3>
                             <dl class="dl-horizontal">
