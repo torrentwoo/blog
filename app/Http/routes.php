9b9a034b1c0bd3b2043edd22459b4b47e1fad0c3
torrentwoo <790896@qq.com>
2017-11-11 23:55:57 +0800

    Assigned namespace for admin group routes, created isolated controllers examples for backend

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -55,7 +55,7 @@ Route::get('/tags/{id}', 'TagsController@show')->name('tag');
 //--------------------------------------/
 //----    Administration routes     ----/
 //--------------------------------------/
-Route::group(['prefix'  =>  'admin', 'middleware'  =>  'auth.admin'], function() {
+Route::group(['prefix'  =>  'admin', 'middleware'  =>  'auth.admin', 'namespace'  =>  'Admin'], function() {
     Route::get('/', function() {
         return 'This is the administration backend';
     });
