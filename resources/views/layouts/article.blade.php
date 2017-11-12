482530c56d6a2a103c66a536a8a40cc58895755b
torrentwoo <790896@qq.com>
2017-11-12 22:30:15 +0800

    Added artiles's user interest system ui

--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -12,8 +12,11 @@
                             <h1>{{ $article->title }}</h1>
                             <ul class="list-inline">
                                 <li>发表于：{{ $article->released_at->diffForHumans() }}</li>
-                                <li>作者：{{ $article->author or 'Admin' }}</li>
+                                <li>作者：{{ $article->author or 'Admin' }}</li><!-- 关注作者 -->
                                 <li>浏览：{{ $article->views }} 次</li>
+                                <li>评论：{{ $article->comments->count() }}</li>
+                                <li>喜欢：</li>
+                                <li>赞赏：</li>
                             </ul>
                         </div>
                         <div class="page-content">
@@ -31,6 +34,50 @@
 --}}
                         </div>
 @endif
+                        <div id="preferences" class="row">
+                            <div id="article-author" class="col-xs-12 col-md-10 col-md-offset-1">
+                                <ul class="media-list">
+                                    <li class="media">
+                                        <div class="media-left">
+                                            <a href="#">
+                                                <img alt="64x64" data-src="holder.js/64x64" class="media-object" style="width: 64px; height: 64px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWY1YjZmN2M2YSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZjViNmY3YzZhIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNCIgeT0iMzYuOCI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" data-holder-rendered="true">
+                                            </a>
+                                        </div>
+                                        <div class="media-body">
+                                            <h2 class="media-heading">Media heading<small><a href="#" class="btn btn-success btn-xs" role="button"><i class="glyphicon glyphicon-plus" aria-hidden="true"></i>关注作者</a></small></h2>
+                                            <p class="text-muted">发表文章：xx 篇，被 xx 人关注，收获 xx 个喜欢</p>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div id="article-rewards" class="col-xs-12 col-md-12 text-center">
+                                <p><a href="javascript:void(0);" class="btn btn-primary btn-sm" role="button">赞赏支持</a></p>
+                                <ul class="list-inline">
+                                    <li>User 1</li>
+                                    <li>User 2</li>
+                                    <li>User 3</li>
+                                </ul>
+                                <hr />
+                            </div>
+                            <p id="article-express" class="col-xs-6 col-md-6 btn-group" role="group">
+                                <a href="#" class="btn btn-danger btn-sm" role="button"><span class="glyphicon glyphicon-heart glyphicon-heart-empty" aria-hidden="true"></span>喜欢</a>
+                                <a href="#" class="btn btn-warning btn-sm" role="button"><span class="glyphicon glyphicon-star glyphicon-star-empty" aria-hidden="true"></span>收藏</a>
+                            </p>
+                            <ul id="article-share" class="col-xs-6 col-md-6 list-inline text-right">
+                                <li><a href="#">微博</a></li>
+                                <li><a href="#">微信</a></li>
+                                <li class="dropup">
+                                    <a href="javascript:void(0);" class="btn btn-default btn-sm dropdown-toggle" data-toggle="dropdown">
+                                        更多分享
+                                        <span class="caret"></span>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-right">
+                                        <li><a href="#">分享到 QQ 空间</a></li>
+                                        <li><a href="#">分享到豆瓣</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                         <nav aria-label="Page navigation">
                             <ul class="pager">
                                 <li class="previous">
