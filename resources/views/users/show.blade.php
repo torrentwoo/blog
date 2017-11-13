fcd0349a222f9023b46dc0d861af4e43bb61d06e
torrentwoo <790896@qq.com>
2017-11-13 15:43:52 +0800

    Added user moments ui

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,12 +1,12 @@
 @extends('shared.singleton')
 
 @section('content')
-            <div class="col-xs-6 col-sm-3 col-sm-offset-1">
+            <div class="col-xs-4 col-sm-3 col-sm-offset-1">
                 <a href="{{ route('user.show', $user->id) }}">
                     <img class="img-rounded img-responsive" src="{{ $user->gravatar(128) }}" alt="{{ $user->name }}" />
                 </a>
             </div>
-            <div class="col-xs-12 col-sm-8">
+            <div class="col-xs-8 col-sm-8">
                 <h1>{{ $user->name }}</h1>
                 <dl>
                     <dd>
@@ -30,14 +30,40 @@
                 </dl>
             </div>
             <!-- 用户动态展示 -->
-            <div class="col-xs-10 col-sm-10 col-sm-offset-1">
-                <div class="row">
-                    <div class="col-xs-2 col-sm-2">
-                        <p>Icon</p>
-                    </div>
-                    <div class="col-xs-10 col-sm-10">
-                        <p>Moment content</p>
-                    </div>
-                </div>
+            <div id="moments" class="col-xs-12 col-sm-10 col-sm-offset-1">
+                <dl class="well my-moment">
+                    <dt>Title.....</dt>
+                    <dd class="occurred"><small>2017-11-11 12AM</small></dd>
+                    <dd>hello world, hello world....</dd>
+                </dl>
+                <dl class="well my-moment">
+                    <dt>Title.....</dt>
+                    <dd class="occurred"><small>2017-11-11 12AM</small></dd>
+                    <dd>hello world, hello world....</dd>
+                </dl>
+                <dl class="well my-moment">
+                    <dt>Title.....</dt>
+                    <dd class="occurred"><small>2017-11-11 12AM</small></dd>
+                    <dd>hello world, hello world....</dd>
+                </dl>
             </div>
+            <nav class="col-xs-12 col-sm-10 col-sm-offset-1 text-center" aria-label="Page navigation">
+                <ul class="pagination">
+                    <li class="disabled">
+                        <a href="#" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li>
+                        <a href="#" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
 @stop
