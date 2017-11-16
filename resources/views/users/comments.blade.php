d8670f57ac7644d0a4d05d5beadea0e6508af851
torrentwoo <790896@qq.com>
2017-11-16 23:47:53 +0800

    User's comments list, tips for data empty

--- resources/views/users/comments.blade.php
+++ resources/views/users/comments.blade.php
@@ -4,47 +4,43 @@
                 <ol class="breadcrumb">
                     <li><a href="{{ route('home') }}">首页</a></li>
                     <li><a href="{{ route('user.show', $user->id) }}">用户</a></li>
-                    <li class="active">我的文章</li>
+                    <li class="active">我的评论</li>
                 </ol>
                 <div class="row">
                     <div class="col-xs-12 col-sm-12">
-@forelse ($favorites as $article)
+@forelse ($comments as $comment)
                         <dl class="well my-moment">
-                            <dt><a href="{{ route('article', $article->id) }}" target="_blank">{{ $article->title }}</a></dt>
+                            <dt><a href="{{ route('article', $comment->article_id) }}" target="_blank">{{ $comment->article->title }}</a></dt>
                             <dd class="occurred">
                                 <small class="text-muted">
                                     <ul class="list-inline">
-                                        <li>创建于：{{ $article->created_at->format('Y-m-d H:i') }}</li>
-                                        <li>阅读：{{ $article->views }}</li>
-                                        <li>评论：{{ $article->comments->count() }}</li>
-                                        <li>喜欢：{{ $article->favorites->count() }}</li>
+                                        <li>评论于：{{ $comment->created_at->format('Y-m-d H:i') }}</li>
+                                        <li>总评论：{{ $comment->article->comments->count() }}</li>
                                     </ul>
                                 </small>
                             </dd>
-                            <dd>{{ $article->description }}</dd>
+                            <dd>{{ $comment->content }}</dd>
                         </dl>
 @empty
                         <div class="alert alert-warning alert-dismissible" role="alert">
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <p><strong>提示：</strong>您还没有收藏过任何内容，不如先去本站的其他栏目逛逛，可能有您喜欢的内容哦</p>
+                            <p><strong>提示：</strong>您还没有发表过任何评论，不如先去本站的其他栏目逛逛，可能有您喜欢的内容哦</p>
                         </div>
 @endforelse
                     </div>
                 </div>
                 <nav class="clearfix text-center" aria-label="Page navigation">
-                    {!! $favorites->render() !!}
+                    {!! $comments->render() !!}
                 </nav>
 @stop
 
 @section('sidebar')
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h5 class="panel-title">猜您喜欢</h5>
+                        <h5 class="panel-title">热门话题</h5>
                     </div>
                     <div class="list-group">
-@foreach ($recommend as $article)
-                        <a class="list-group-item" href="{{ route('article', $article->id) }}">{{ $article->title }}</a>
-@endforeach
+                        <p class="list-group-item">暂时还未出现热门话题，不如<a href="#">去创建一个</a></p>
                     </div>
                 </div>
 @stop
