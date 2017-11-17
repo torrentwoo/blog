918bd926bcf0cecf27c94fc0b99c1b33e7ffe409
torrentwoo <790896@qq.com>
2017-11-17 20:42:19 +0800

    Column layout altered

--- resources/views/layouts/column.blade.php
+++ resources/views/layouts/column.blade.php
@@ -5,42 +5,96 @@
                     <li><a href="{{ route('home') }}">首页</a></li>
                     <li class="active">{{ $column->name }}</li>
                 </ol>
-                <p>栏目简介……</p>
+                <div class="media" id="column-header">
+                    <div class="media-left">
+                        <a href="#">
+                            <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 64px; height: 64px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjOWRmYjc3NCB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM5ZGZiNzc0Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNC41IiB5PSIzNS42Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true">
+                        </a>
+                    </div>
+                    <div class="media-body">
+                        <h1 class="media-heading h2">{{ $column->name }}</h1>
+                        <p>收录文章：{{ $column->articles()->released()->count() }} 篇</p>
+                    </div>
+                    <div class="media-right">
+                        <a href="#">
+                            <img class="media-object" data-src="holder.js/64x64" alt="64x64" style="width: 64px; height: 64px;" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjOWRmOTZiZSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM5ZGY5NmJlIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNC41IiB5PSIzNS42Ij42NHg2NDwvdGV4dD48L2c+PC9nPjwvc3ZnPg==" data-holder-rendered="true">
+                        </a>
+                        <a href="#">
+                            加关注
+                        </a>
+                    </div>
+                </div>
                 <div class="row">
                     <div class="col-xs-12 col-sm-12">
-                        <ul id="myTabs" class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#home" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">Home</a></li>
-                            <li role="presentation"><a aria-expanded="false" href="#profile" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile">Profile</a></li>
-                            <li role="presentation"><a aria-expanded="false" href="#third" role="tab" id="third-tab" data-toggle="tab" aria-controls="third">Profile</a></li>
+                        <ul class="nav nav-tabs" id="column-tabs">
+                            <li role="presentation"><a href="#"><span class="glyphicon glyphicon-comment" aria-hidden="true"></span>新近评论</a></li>
+                            <li role="presentation" class="active"><a href="#"><span class="glyphicon glyphicon-bullhorn" aria-hidden="true"></span>最新发表</a></li>
+                            <li role="presentation"><a href="#"><span class="glyphicon glyphicon-fire" aria-hidden="true"></span>栏目热门</a></li>
                         </ul>
-                        <div id="myTabContent" class="tab-content">
-                            <div role="tabpanel" class="tab-pane fade active in" id="home" aria-labelledby="home-tab">
-                                <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
-                            </div>
-                            <div role="tabpanel" class="tab-pane fade" id="profile" aria-labelledby="profile-tab">
-                                <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit. Keytar helvetica VHS salvia yr, vero magna velit sapiente labore stumptown. Vegan fanny pack odio cillum wes anderson 8-bit, sustainable jean shorts beard ut DIY ethical culpa terry richardson biodiesel. Art party scenester stumptown, tumblr butcher vero sint qui sapiente accusamus tattooed echo park.</p>
-                            </div>
-                            <div role="tabpanel" class="tab-pane fade" id="third" aria-labelledby="dropdown1-tab">
-                                <p>Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork. Williamsburg banh mi whatever gluten-free, carles pitchfork biodiesel fixie etsy retro mlkshk vice blog. Scenester cred you probably haven't heard of them, vinyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
-                            </div>
-                        </div>
-                    </div>
 @foreach ($articles as $article)
-                    <div class="col-sm-6 col-md-4">
-                        <div class="thumbnail">
-                            <a href="{{ route('article', $article->id) }}" title="{{ $article->title }}">
-                                <img class="img-responsive" src="{{ $article->snapshot->thumbnail_url or $article->attachment->url }}" alt="{{ $article->title }}" />
-                            </a>
-                            <div class="caption">
-                                <h2>{{ $article->title }}</h2>
-                                <p>{{ $article->description }}</p>
-                                <p class="text-right"><a class="btn btn-default" href="{{ route('article', $article->id) }}" role="button">View details &raquo;</a></p>
+                        <div class="media media-article">
+                            <div class="media-left">
+                                <a href="{{ route('article', $article->id) }}">
+                                    <img alt="{{ $article->title }}" data-src="holder.js/150x120" class="media-object media-preview" src="{{  $article->snapshot->thumbnail_url or $article->attachment->url }}" data-holder-rendered="true" />
+                                </a>
                             </div>
+                            <div class="media-body">
+                                <h2 class="h4 media-heading media-title">
+                                    <a href="{{ route('article', $article->id) }}" title="{{ $article->title }}">{{ $article->title }}</a>
+                                </h2>
+                                <ul class="list-inline text-muted media-author">
+                                    <li><a href="{{ route('user.show', $article->author->id) }}" class="text-muted">
+                                            <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                            <span class="sr-only">作者：</span>
+                                            {{ $article->author->name }}
+                                        </a>
+                                    </li>
+                                    <li><span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+                                        <span class="sr-only">文章发布在：</span>
+                                        {{ $article->released_at->diffForHumans() }}
+                                    </li>
+                                </ul>
+                                <p>{{ $article->description }}</p>
+                                <ul class="list-inline media-meta">
+                                    <li>
+                                        <a class="text-muted" href="{{ route('article', $article->id) }}">
+                                            <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                                            <span class="sr-only">浏览：</span>
+                                            {{ $article->views }}
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a class="text-muted" href="{{ route('article', $article->id) . '#comments' }}">
+                                            <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+                                            <span class="sr-only">评论：</span>
+                                            {{ $article->comments->count() }}
+                                        </a>
+                                    </li>
+                                    <li>
+                                    <span class="text-muted">
+                                        <span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+                                        <span class="sr-only">喜欢：</span>
+                                        {{ $article->favorites()->likes()->count() }}
+                                    </span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <hr />
                         </div>
-                    </div>
 @endforeach
+                    </div>
                 </div>
                 <nav class="text-center" aria-label="Page navigation">
                     {!! $articles->render() !!}
                 </nav>
+@stop
+
+@section('sidebar')
+                <h5 class="text-muted">栏目简介</h5>
+                <p>aa</p>
+                <p class="h5">投稿须知</p>
+
+                <hr />
+
+                <p>高产作者</p>
 @stop
