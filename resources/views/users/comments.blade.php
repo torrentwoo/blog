388558dd1ced93379a9fc89f9070f5d1b375bd5a
torrentwoo <790896@qq.com>
2017-11-22 15:39:26 +0800

    Comments data rendering

--- resources/views/users/comments.blade.php
+++ resources/views/users/comments.blade.php
@@ -3,14 +3,87 @@
 @section('subContent')
                     <div class="col-xs-12">
                         <ul class="nav nav-tabs" id="inline-menu">
-                            <li role="presentation" class="active"><a href="#oneself"><i class="glyphicon glyphicon-flag" aria-hidden="true"></i>我的<span class="hidden-xs">评论</span></a></li>
-                            <li role="presentation"><a href="#others"><i class="glyphicon glyphicon-comment" aria-hidden="true"></i>评论<span class="hidden-xs">我的</span></a></li>
+                            <li role="presentation" class="active">
+                                <a href="#oneself" id="oneself-tab" data-toggle="tab" aria-controls="oneself" aria-expanded="true" role="tab">
+                                    <i class="glyphicon glyphicon-flag" aria-hidden="true"></i>我的<span class="hidden-xs">评论</span>
+                                </a>
+                            </li>
+                            <li role="presentation">
+                                <a href="#others" id="others-tab" data-toggle="tab" aria-controls="others" aria-expanded="false" role="tab">
+                                    <i class="glyphicon glyphicon-comment" aria-hidden="true"></i>评论<span class="hidden-xs">我的</span>
+                                </a>
+                            </li>
                         </ul>
-                    </div>
-                    <div class="col-xs-12">
-                        <div class="alert alert-warning alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <p><strong>提示：</strong>您还没有发表过任何评论，不如先去本站的其他栏目逛逛，可能有您感兴趣内容哦</p>
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane fade active in" id="oneself" aria-labelledby="oneself-tab">
+@forelse ($oneself as $comment)
+                                <div class="well well-quirk">
+                                    <ul class="list-inline">
+                                        <li><a href="{{ route('user.show', $user->id) }}">{{ $user->name }}</a></li>
+                                        <li>@if (!$comment->parent_id)发表@else回复@endif了评论</li>
+                                        <li class="text-muted">{{ $comment->created_at->format('n/j h:s a') }}</li>
+                                    </ul>
+                                    <p>{{ $comment->content }}</p>
+                                    <blockquote class="small text-muted">
+@if ($comment->commentable_type === \App\Models\Article::class)
+                                        <h4><a href="{{ route('article.show', $comment->commentable->id) }}">{{ $comment->commentable->title }}</a></h4>
+                                        <p>{{ str_limit($comment->commentable->content, 320) }}</p>
+                                        <ul class="list-inline text-muted">
+                                            <li><a href="{{ route('user.show', $comment->commentable->author->id) }}">{{ $comment->commentable->author->name }}</a></li>
+                                            <li>
+                                                <a class="text-muted" href="{{ route('article.show', $comment->commentable->id) }}">
+                                                    <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                                                    <span class="sr-only">浏览：</span>
+                                                    {{ $comment->commentable->views }}
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a class="text-muted" href="{{ route('article.show', $comment->commentable->id) . '#comments' }}">
+                                                    <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+                                                    <span class="sr-only">评论：</span>
+                                                    {{ $comment->commentable->comments->count() }}
+                                                </a>
+                                            </li>
+                                            <li><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+                                                <span class="sr-only">喜欢：</span>
+                                                {{ $comment->commentable->likes->count() }}
+                                            </li>
+                                        </ul>
+@elseif ($comment->commentable_type === \App\Models\Comment::class)
+                                        <p>{{ str_limit($comment->commentable->content, 320) }}</p>
+                                        <ul class="list-inline text-muted">
+                                            <li><a href="{{ route('user.show', $comment->commentable->user->id) }}">{{ $comment->commentable->user->name }}</a></li>
+                                            <li>
+                                                <span class="glyphicon glyphicon-thumbs-up" aria-hidden="true"></span>
+                                                <span class="sr-only">赞：</span>
+                                                0
+                                            </li>
+                                            <li>
+                                                <span class="glyphicon glyphicon-thumbs-down" aria-hidden="true"></span>
+                                                <span class="sr-only">踩：</span>
+                                                0
+                                            </li>
+                                        </ul>
+@endif
+                                    </blockquote>
+                                </div>
+@empty
+                                <div class="alert alert-warning alert-dismissible" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <p><strong>提示：</strong>您还没有发表过任何评论，不如先去本站的其他栏目逛逛，可能有您感兴趣内容哦</p>
+                                </div>
+@endforelse
+                            </div>
+
+                            <div role="tabpanel" class="tab-pane fade" id="others" aria-labelledby="others-tab">
+@forelse ($others as $comment)
+@empty
+                                <div class="alert alert-warning alert-dismissible" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <p><strong>提示：</strong>您还没有得到过任何评论哦</p>
+                                </div>
+@endforelse
+                            </div>
                         </div>
                     </div>
 @stop
--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -3,14 +3,117 @@
 @section('subContent')
                     <div class="col-xs-12">
                         <ul class="nav nav-tabs" id="inline-menu">
-                            <li role="presentation"><a href="#liked"><i class="glyphicon glyphicon-heart" aria-hidden="true"></i><span class="hidden-xs">我</span>喜欢<span class="hidden-xs">的</span></a></li>
-                            <li role="presentation" class="active"><a href="#favorites"><i class="glyphicon glyphicon-bookmark" aria-hidden="true"></i><span class="hidden-xs">我</span>收藏<span class="hidden-xs">的</span></a></li>
+                            <li role="presentation">
+                                <a href="#liked" id="liked-tab" data-toggle="tab" aria-controls="liked" aria-expanded="false" role="tab">
+                                    <i class="glyphicon glyphicon-heart" aria-hidden="true"></i><span class="hidden-xs">我</span>喜欢<span class="hidden-xs">的</span>
+                                </a>
+                            </li>
+                            <li role="presentation" class="active">
+                                <a href="#favorites" id="favorites-tab" data-toggle="tab" aria-controls="favorites" aria-expanded="true" role="tab">
+                                    <i class="glyphicon glyphicon-bookmark" aria-hidden="true"></i><span class="hidden-xs">我</span>收藏<span class="hidden-xs">的</span>
+                                </a>
+                            </li>
                         </ul>
-                    </div>
-                    <div class="col-xs-12">
-                        <div class="alert alert-warning alert-dismissible" role="alert">
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <p><strong>提示：</strong>您还没有收藏过任何内容，不如先去本站的其他栏目逛逛，可能有您感兴趣内容哦</p>
+                        <div class="tab-content">
+                            <div role="tabpanel" class="tab-pane fade" id="liked" aria-labelledby="liked-tab">
+@forelse ($liked as $article)
+                                <div class="media media-article">
+@if (!$article->thumbnails->isEmpty())
+                                    <div class="media-left hidden-portrait">
+                                        <a href="{{ route('article.show', $article->id) }}">
+                                            <img alt="{{ $article->title }}" data-src="holder.js/150x120" class="media-object media-preview" src="{{  $article->thumbnails->first()->url }}" data-holder-rendered="true" />
+                                        </a>
+                                    </div>
+@endif
+                                    <div class="media-body">
+                                        <h4 class="media-heading media-title">
+                                            <a href="{{ route('article.show', $article->id) }}" title="{{ $article->title }}">{{ $article->title }}</a>
+                                        </h4>
+                                        <p>{{ $article->description }}</p>
+                                        <ul class="list-inline text-muted media-meta">
+                                            <li>
+                                                <span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+                                                <span class="sr-only">文章发布日期：</span>
+                                                {{ $article->released_at->diffForHumans() }}
+                                            </li>
+                                            <li>
+                                                <a class="text-muted" href="{{ route('article.show', $article->id) }}">
+                                                    <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                                                    <span class="sr-only">浏览：</span>
+                                                    {{ $article->views }}
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a class="text-muted" href="{{ route('article.show', $article->id) . '#comments' }}">
+                                                    <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+                                                    <span class="sr-only">评论：</span>
+                                                    {{ $article->comments->count() }}
+                                                </a>
+                                            </li>
+                                            <li><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+                                                <span class="sr-only">喜欢：</span>
+                                                {{ $article->likes->count() }}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+@empty
+                                <div class="alert alert-warning alert-dismissible" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <p><strong>提示：</strong>您还没有喜欢过任何内容，不如先去本站的其他栏目逛逛，可能有您喜欢的内容哦</p>
+                                </div>
+@endforelse
+                            </div>
+
+                            <div role="tabpanel" class="tab-pane fade active in" id="favorites" aria-labelledby="favorites-tab">
+@forelse ($favorites as $article)
+                                <div class="media media-article">
+@if (!$article->thumbnails->isEmpty())
+                                    <div class="media-left hidden-portrait">
+                                        <a href="{{ route('article.show', $article->id) }}">
+                                            <img alt="{{ $article->title }}" data-src="holder.js/150x120" class="media-object media-preview" src="{{  $article->thumbnails->first()->url }}" data-holder-rendered="true" />
+                                        </a>
+                                    </div>
+@endif
+                                    <div class="media-body">
+                                        <h4 class="media-heading media-title">
+                                            <a href="{{ route('article.show', $article->id) }}" title="{{ $article->title }}">{{ $article->title }}</a>
+                                        </h4>
+                                        <p>{{ $article->description }}</p>
+                                        <ul class="list-inline text-muted media-meta">
+                                            <li>
+                                                <span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+                                                <span class="sr-only">文章发布日期：</span>
+                                                {{ $article->released_at->diffForHumans() }}
+                                            </li>
+                                            <li>
+                                                <a class="text-muted" href="{{ route('article.show', $article->id) }}">
+                                                    <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                                                    <span class="sr-only">浏览：</span>
+                                                    {{ $article->views }}
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a class="text-muted" href="{{ route('article.show', $article->id) . '#comments' }}">
+                                                    <span class="glyphicon glyphicon-comment" aria-hidden="true"></span>
+                                                    <span class="sr-only">评论：</span>
+                                                    {{ $article->comments->count() }}
+                                                </a>
+                                            </li>
+                                            <li><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
+                                                <span class="sr-only">喜欢：</span>
+                                                {{ $article->likes->count() }}
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+@empty
+                                <div class="alert alert-warning alert-dismissible" role="alert">
+                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <p><strong>提示：</strong>您还没有收藏过任何内容，不如先去本站的其他栏目逛逛，可能有您感兴趣内容哦</p>
+                                </div>
+@endforelse
+                            </div>
                         </div>
                     </div>
 @stop
