36e999c29673930e2f86c5e1c6d50f9e4360af65
torrentwoo <790896@qq.com>
2017-11-23 18:19:47 +0800

    Update subscription templates, data rendering

--- resources/views/subscriptions/index.blade.php
+++ resources/views/subscriptions/index.blade.php
@@ -2,23 +2,49 @@
 
 @section('rightContent')
             <div class="col-xs-12 col-sm-8">
+@if ($followings->first()->followable_type === App\Models\User::class)
                 <div class="media header-media">
                     <div class="media-left">
-                        <a href="#">
-                            <img class="img-rounded" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWY1YjZmN2M2YSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZjViNmY3YzZhIj48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNCIgeT0iMzYuOCI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" />
+                        <a href="{{ route('user.show', $origin->id) }}">
+                            <img class="img-rounded" src="{{ $origin->gravatar(64) }}" />
                         </a>
                     </div>
                     <div class="media-body">
-                        <h1 class="media-heading h2">主标题</h1>
-                        <p>收录文章xxxx篇<i class="divider"></i>有xxxx人关注</p>
+                        <h1 class="media-heading h2">{{ $origin->name }}</h1>
+                        <p>发表文章{{ $origin->articles->count() }}篇<i class="divider">&middot;</i>获得{{ $origin->likes->count() }}个喜欢</p>
                     </div>
                     <div class="media-right nowrap-landscape" id="user-buttons">
-                        <button type="button" class="btn btn-info btn-xs">投稿</button>
-                        <button type="button" class="btn btn-success btn-xs">
-                            <i class="glyphicon glyphicon-minus glyphicon-plus" aria-hidden="true"></i>关注
+                        <button type="button" class="btn btn-info btn-xs">
+                            <i class="glyphicon glyphicon-send" aria-hidden="true"></i>发站内信
                         </button>
+                        <a href="{{ route('user.show', $origin->id) }}" class="btn btn-success btn-xs" role="button">
+                            个人主页<span class="glyphicon glyphicon-chevron-right offset-right" aria-hidden="true"></span>
+                        </a>
                     </div>
                 </div>
+@elseif ($followings->first()->followable_type === App\Models\Column::class)
+                <div class="media header-media">
+@if (!$origin->thumbnails->isEmpty())
+                    <div class="media-left">
+                        <a href="{{ route('column.show', $origin->id) }}">
+                            <img class="img-rounded" src="{{ $origin->thumbnails->first()->url }}" />
+                        </a>
+                    </div>
+@endif
+                    <div class="media-body">
+                        <h1 class="media-heading h2">{{ $origin->name }}</h1>
+                        <p>收录文章{{ $origin->articles->count() }}篇<i class="divider">&middot;</i>有{{ $origin->follows->count() }}人关注</p>
+                    </div>
+                    <div class="media-right nowrap-landscape" id="user-buttons">
+                        <button type="button" class="btn btn-info btn-xs">
+                            <i class="glyphicon glyphicon-ok-circle" aria-hidden="true"></i>投稿
+                        </button>
+                        <a href="{{ route('column.show', $origin->id) }}" class="btn btn-success btn-xs" role="button">
+                            栏目主页<span class="glyphicon glyphicon-chevron-right offset-right" aria-hidden="true"></span>
+                        </a>
+                    </div>
+                </div>
+@endif
                 <ul class="nav nav-tabs" id="inline-menu">
                     <li role="presentation" class="active">
                         <a href="#latest" id="latest-tab" data-toggle="tab" aria-controls="latest" aria-expanded="true" role="tab">
