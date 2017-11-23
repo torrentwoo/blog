c521732bd470cf111d33b510bbeeeb26f2d0af41
torrentwoo <790896@qq.com>
2017-11-23 17:01:57 +0800

    Update subscriptions controller, method index finished

--- app/Http/Controllers/SubscriptionsController.php
+++ app/Http/Controllers/SubscriptionsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class SubscriptionsController extends Controller
 {
@@ -16,7 +17,31 @@ class SubscriptionsController extends Controller
 
     public function index()
     {
-        return view('subscriptions.index')->with('subscriptionActive', 'active');
+        // Get following list
+        $followings = Auth::user()->follows()->get();
+        // Get the first element, retrieve its data, to put them into default view
+        $first = $followings->first()->followable;
+        // The latest based on the first
+        $latest = $first->articles()->with('author')->released()->latest('released_at')->get();
+        // The most commented --articles-- based on the first
+        $commented = $first->articles()->released()->with('author', 'comments')->get()->filter(function($item) {
+            return $item->comments->isEmpty() !== true;
+        })->sortByDesc('comments')->values();
+        // The popular --articles-- based on the first
+        $popular = $first->articles()->released()->with('author', 'likes', 'comments')->get()->sort(function($a, $b) {
+            if ($a->likes->count() === $b->likes->count()) {
+                return 0;
+            }
+            return $a->views + $a->comments->count() < $b->views + $b->comments->count() ? -1 : 1;
+        })->values();
+
+        return view('subscriptions.index', [
+            'followings'    =>  $followings,
+            'first'         =>  $first,
+            'latest'        =>  $latest,
+            'commented'     =>  $commented,
+            'popular'       =>  $popular,
+        ])->with('subscriptionActive', 'active');
     }
 
     public function recommend()
