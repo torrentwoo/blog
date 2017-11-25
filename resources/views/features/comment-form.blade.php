87690cae67a83e598af7bb1a32bb89fa08b44aec
torrentwoo <790896@qq.com>
2017-11-26 02:37:45 +0800

    Update features/comment-form.blade.php, simplified the form

--- resources/views/features/comment-form.blade.php
+++ resources/views/features/comment-form.blade.php
@@ -2,36 +2,17 @@
                             @include('features.builtIn-alert')
 
                             {{ csrf_field() }}
+@if (Auth::check())
                             <div class="form-group">
                                 <textarea name="comment" class="form-control" rows="3" placeholder="请在此发表您的评论"></textarea>
                             </div>
-@if (Auth::check())
                             <div class="form-group text-right">
                                 <button type="submit" class="btn btn-default">发表评论</button>
                             </div>
 @else
-@if (isset($modalLogin) && $modalLogin)
-                            <div class="form-group text-right">
-                                <button type="button" class="btn btn-default" data-toggle="modal" data-target="#loginModal" data-trigger="#embeddedCommentForm">登录发表评论</button>
+                            <div class="well text-center">
+                                <a href="{{ route('login') }}?redirect_back={{ URL::current() }}" class="btn btn-primary" role="button">登录</a>
+                                <span class="offset-right">后发表评论</span>
                             </div>
-@else
-                            <div class="form-group form-inline">
-                                <div class="form-group">
-                                    <label for="username">帐号</label>
-                                    <input type="text" name="username" id="username" class="form-control" placeholder="您的帐号" />
-                                </div>
-                                <div class="form-group">
-                                    <label for="password">密码</label>
-                                    <input type="password" name="password" id="password" class="form-control" placeholder="登录密码" />
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" />
-                                        <span>记住我（下次自动登录）</span>
-                                    </label>
-                                </div>
-                                <button type="submit" class="btn btn-default pull-right">登录并发表</button>
-                            </div>
-@endif
 @endif
                         </form>
