185c0b27547e841552a1ba7c83c7d0027081c495
torrentwoo <790896@qq.com>
2017-12-04 11:26:52 +0800

    Update message show conversation view, data binding

--- resources/views/messages/show.blade.php
+++ resources/views/messages/show.blade.php
@@ -4,49 +4,37 @@
                 <div class="row">
                     <div class="col-xs-12 col-md-12">
                         <div class="page-header">
-                            <h1>站内信<small class="offset-right">与 xxx 的会话</small>
+                            <h1>站内信<small class="offset-right">与 {{ $import->name }} 的会话</small>
                                 <a class="offset-right header-back" href="{{ route('message.index') }}">
                                     <i class="glyphicon glyphicon-menu-left" aria-hidden="true"></i>返回
                                 </a>
                             </h1>
                         </div>
                         <div class="dialog-box">
+@foreach ($messages as $message)
+@if ($message->sender->id === $import->id)
                             <div class="dialog-import">
                                 <div class="dialog-content">
-                                    <img class="img-circle avatar-xs dialog-avatar" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjOGY1M2UwOSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM4ZjUzZTA5Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNCIgeT0iMzYuOCI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" />
-                                    <div class="well dialog-message">这里是传入的内容这里是传入的内容这里是传入的内容这里是传入的内容这里是传入的内容</div>
-                                    <small class="text-muted">2017-10-11 12:21</small>
+                                    <img class="img-circle avatar-xs dialog-avatar" src="{{ $message->sender->gravatar(32) }}" />
+                                    <div class="well dialog-message">{{ $message->content }}</div>
+                                    <small class="text-muted">{{ $message->created_at->format('n/j g:i a') }}</small>
                                 </div>
                             </div>
+@else
                             <div class="dialog-export">
                                 <div class="dialog-content">
-                                    <img class="img-circle avatar-xs dialog-avatar" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjOGY1M2UwOSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM4ZjUzZTA5Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNCIgeT0iMzYuOCI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" />
-                                    <div class="well dialog-message">
-                                        这里是输出的内容
-                                    </div>
-                                    <small class="dialog-timestamp text-muted">2017-10-11 12:21</small>
+                                    <img class="img-circle avatar-xs dialog-avatar" src="{{ $message->sender->gravatar(32) }}" />
+                                    <div class="well dialog-message">{{ $message->content }}</div>
+                                    <small class="dialog-timestamp text-muted">{{ $message->created_at->format('n/j g:i a') }}</small>
                                 </div>
                             </div>
-                            <div class="dialog-import">
-                                <div class="dialog-content">
-                                    <img class="img-circle avatar-xs dialog-avatar" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjOGY1M2UwOSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM4ZjUzZTA5Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNCIgeT0iMzYuOCI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" />
-                                    <div class="well dialog-message">这里是传入的内容这里是传入的内容这里是传入的内容这里是传入的内容这里是传入的内容这里是传入的内容这里是传入的内容这里是传入的内容</div>
-                                    <small class="text-muted">2017-10-11 12:21</small>
-                                </div>
-                            </div>
-                            <div class="dialog-export">
-                                <div class="dialog-content">
-                                    <img class="img-circle avatar-xs dialog-avatar" src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9InllcyI/PjxzdmcgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIHZpZXdCb3g9IjAgMCA2NCA2NCIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+PCEtLQpTb3VyY2UgVVJMOiBob2xkZXIuanMvNjR4NjQKQ3JlYXRlZCB3aXRoIEhvbGRlci5qcyAyLjYuMC4KTGVhcm4gbW9yZSBhdCBodHRwOi8vaG9sZGVyanMuY29tCihjKSAyMDEyLTIwMTUgSXZhbiBNYWxvcGluc2t5IC0gaHR0cDovL2ltc2t5LmNvCi0tPjxkZWZzPjxzdHlsZSB0eXBlPSJ0ZXh0L2NzcyI+PCFbQ0RBVEFbI2hvbGRlcl8xNWZjOGY1M2UwOSB0ZXh0IHsgZmlsbDojQUFBQUFBO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1mYW1pbHk6QXJpYWwsIEhlbHZldGljYSwgT3BlbiBTYW5zLCBzYW5zLXNlcmlmLCBtb25vc3BhY2U7Zm9udC1zaXplOjEwcHQgfSBdXT48L3N0eWxlPjwvZGVmcz48ZyBpZD0iaG9sZGVyXzE1ZmM4ZjUzZTA5Ij48cmVjdCB3aWR0aD0iNjQiIGhlaWdodD0iNjQiIGZpbGw9IiNFRUVFRUUiLz48Zz48dGV4dCB4PSIxNCIgeT0iMzYuOCI+NjR4NjQ8L3RleHQ+PC9nPjwvZz48L3N2Zz4=" />
-                                    <div class="well dialog-message">
-                                        这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容这里是输出的内容
-                                    </div>
-                                    <small class="dialog-timestamp text-muted">2017-10-11 12:21</small>
-                                </div>
-                            </div>
+@endif
+@endforeach
                         </div>
                         <div class="dialog-form">
-                            <form method="POST" action="#">
+                            <form method="POST" action="{{ route('message.send', $import->id) }}">
                                 @include('features.builtIn-alert')
+
                                 {{ csrf_field() }}
                                 <div class="form-group">
                                     <textarea name="comment" class="form-control" rows="3" placeholder="请在此输入站内信"></textarea>
