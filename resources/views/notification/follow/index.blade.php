82975fb796db4abebb58f460e4f6618b22fb346f
torrentwoo <790896@qq.com>
2017-12-07 23:08:59 +0800

    Update the fore-end of follow notification

--- resources/views/notification/follow/index.blade.php
+++ resources/views/notification/follow/index.blade.php
@@ -4,11 +4,33 @@
                 <div class="row">
                     <div class="col-xs-12 col-md-12">
                         <div class="page-header">
-                            <h1>收到的关注<small class="offset-right">全部</small></h1>
+                            <h1>被关注<small class="offset-right">全部</small></h1>
                         </div>
 @forelse ($follows as $notification)
-                        <div>
-                            某某关注了您
+                        <div class="media media-quirk" id="notification{{ $notification->id }}">
+                            <div class="media-left">
+                                <img class="media-object img-circle avatar-sm" src="{{ $notification->notifiable->holder->gravatar(48) }}" />
+                            </div>
+                            <div class="media-body">
+                                <h3 class="media-heading h4">{{ $notification->subject }}
+                                    <small class="offset-right">{{ $notification->notifiable->created_at->format('Y-m-d g:i a') }}</small>
+                                </h3>
+                                <p class="text-muted">{{ $notification->content }}</p>
+                            </div>
+                            <div class="media-right">
+                                <div class="dropdown">
+                                    <a href="javascript:void(0);" class="btn btn-xs" id="dropdownMenu{{ $notification->id }}" data-toggle="dropdown" role="button">
+                                        <span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span>
+                                        <span class="sr-only">操作：</span>
+                                    </a>
+                                    <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu{{ $notification->id }}">
+                                        <li><a class="caution bg-primary" href="{{ route('notification.showFollow', $notification->id) }}"><i class="glyphicon glyphicon-eye-open" aria-hidden="true"></i>查看</a></li>
+                                        <li><a class="caution bg-danger" href="javascript:void(0);" data-toggle="#notification{{ $notification->id }}" data-handler="{{ route('notification.deleteFollow', $notification->id) }}"><i class="glyphicon glyphicon-trash" aria-hidden="true"></i>删除</a></li>
+                                        <li role="separator" class="divider"></li>
+                                        <li><a class="caution bg-danger" href="#blacklist-{{ $notification->notifiable->holder->id }}"><i class="glyphicon glyphicon-ban-circle" aria-hidden="true"></i>加入黑名单</a></li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
 @empty
                         <div class="alert alert-warning alert-dismissible" role="alert">
@@ -16,29 +38,40 @@
                             <p><strong>提示：</strong>您还没有收到任何此类消息通知</p>
                         </div>
 @endforelse
-                        <nav class="text-center" aria-label="Page navigation">
-                            <ul class="pagination">
-                                <li class="disabled">
-                                    <a href="#" aria-label="Previous">
-                                        <span aria-hidden="true">&laquo;</span>
-                                    </a>
-                                </li>
-                                <li class="active"><a href="#">1</a></li>
-                                <li><a href="#">2</a></li>
-                                <li><a href="#">3</a></li>
-                                <li><a href="#">4</a></li>
-                                <li><a href="#">5</a></li>
-                                <li>
-                                    <a href="#" aria-label="Next">
-                                        <span aria-hidden="true">&raquo;</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </nav>
                     </div>
                 </div>
 @stop
 
 @section('sidebar')
                 @include('notification.menu')
+@stop
+
+@section('scripts')
+    <script type="text/javascript">
+        $(function() {
+            // Delete notification
+            $('.media-quirk').on('click', 'a[data-toggle][data-handler]', function() {
+                if (!confirm('是否确认删除这条通知消息')) {
+                    return false;
+                }
+                var $btn = $(this);
+                var $wrap = $($btn.data('toggle'));
+                var $url = $btn.data('handler');
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                $.post($url, {
+                    '_method': 'DELETE'
+                }, function(response) {
+                    if (!response.error) {
+                        $wrap.remove();
+                    } else {
+                        window.alert(response.message);
+                    }
+                }, 'json');
+            });
+        });
+    </script>
 @stop
---
+++ resources/views/notification/follow/show.blade.php
@@ -0,0 +1,43 @@
+@extends('shared.prototype')
+
+@section('content')
+                <div class="row">
+                    <div class="col-xs-12 col-md-12">
+                        <div class="page-header">
+                            <h1>被关注<small class="offset-right">详情</small>
+                                <a class="offset-right header-back" href="{{ route('notification.follow') }}">
+                                    <i class="glyphicon glyphicon-menu-left" aria-hidden="true"></i>返回
+                                </a>
+                            </h1>
+                        </div>
+                        <div class="well well-quirk">
+                            <ul class="list-inline">
+                                <li><img class="img-circle avatar-xs" src="{{ $notification->notifiable->holder->gravatar(32) }}" /></li>
+                                <li>{{ $notification->notifiable->holder->name }}</li>
+                                <li>关注了您</li>
+                                <li class="small text-muted">{{ $notification->notifiable->created_at->format('Y-m-d g:i a') }}</li>
+                            </ul>
+                            <blockquote class="small text-muted">
+                                <div class="well media">
+                                    <div class="media-left">
+                                        <a href="{{ route('user.show', $notification->notifiable->holder->id) }}">
+                                            <img class="media-object img-circle avatar-sm" src="{{ $notification->notifiable->holder->gravatar(48) }}" />
+                                        </a>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">{{ $notification->notifiable->holder->name }}</h4>
+                                        <p class="text-muted">发布了 {{ $notification->notifiable->holder->articles->count() }} 篇文章，被 {{ $notification->notifiable->holder->followingUsers->count() }} 人关注，获得 {{ $notification->notifiable->holder->likedUsers()->count() }} 个喜欢</p>
+                                    </div>
+@if ($notification->notifiable->holder->introduction)
+                                    <div class="media-bottom text-muted">{{ $notification->notifiable->holder->introduction }}</div>
+@endif
+                                </div>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+@stop
+
+@section('sidebar')
+                @include('notification.menu')
+@stop
