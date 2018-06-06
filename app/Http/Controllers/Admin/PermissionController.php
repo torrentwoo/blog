0dbd74226e23d2694f527fbf08394c638ad9837c
Torrent Woo <790896@qq.com>
2018-06-06 21:12:54 +0800

    Refactored two of admin's controllers

--- app/Http/Controllers/Admin/PermissionController.php
+++ app/Http/Controllers/Admin/PermissionController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class RolesController extends Controller
+class PermissionController extends Controller
 {
     /**
      * Display a listing of the resource.
